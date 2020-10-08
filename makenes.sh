ebd2ac9fe3e46d2611d6bbc243a11a9052132a57
urbster1 <urbster1@users.noreply.github.com>
2020-10-08 10:25:50 -0400

    Port to Bash

---
+++ makenes.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set FILE=$1
+set PNG=${FILE}.png
+python3 ~/makechr/makechr.py ${PNG} -o ${FILE}.dat
+if [[ -f ${FILE}palette.dat && --f ${FILE}attribute.dat -f ${FILE}nametable.dat -f ${FILE}chr.dat ]]; then
+  python3 glitch.py ${FILE}
+  truncate -c -s 4k ${FILE}chr.dat
+  cat ${FILE}chr.dat ${FILE}chr.dat ${FILE}chr.dat ${FILE}chr.dat ${FILE}chr.dat ${FILE}chr.dat ${FILE}chr.dat ${FILE}chr.dat > ${FILE}.chr
+  truncate -c -s 16 ${FILE}palette.dat
+  mv ${FILE}palette.dat ${FILE}.pal
+  cat ${FILE}nametable.dat ${FILE}attribute.dat > ${FILE}.dat
+  ./asm6 ${FILE}.asm ${FILE}.bin
+  cat ${FILE}.bin ${FILE}.chr > ${FILE}.nes
+  rm ${FILE}.bin
+else
+  echo Could not find binary files from ${PNG}. Check to make sure makechr is working.
+fi
